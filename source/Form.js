24cb1acfb0c49c70f15a08979bf2fd1645985737
arxii <yurisido@gmail.com>
2016-05-12 20:37:26 -0500

    remove accidental jquery dependency

--- source/Form.js
+++ source/Form.js
@@ -2,7 +2,7 @@
 var React = require('react');
 var I = require('./Slide');
 var Mixin = require('./Mixin');
-var $ = require('jquery');
+
 
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
